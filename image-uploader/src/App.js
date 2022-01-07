b31e570045835c06e43e11b13291b1af92667010
Antonio <ditacj78@gmail.com>
2022-01-07 19:17:47 +0100

    App.js: added texts and using bootstrap styles

--- image-uploader/src/App.js
+++ image-uploader/src/App.js
@@ -1,14 +1,19 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { FileUpload } from './components/FileUpload';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <div>
+    <div className='root'>
+      <h2 className='text-center m-5 text-light'>The File Uploader</h2>
+      <p className='text-center m-5 text-light'>(With React.js, Express & Multer)</p>
+      <div className='file-upload-wrapper'>
+        <div className='result'></div>
         <FileUpload />
       </div>
     </div>
   );
 }
+
 
 export default App;
